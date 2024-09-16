951bc78a77dc848951bbdf025bbd3fe547f1d161
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-09-16 11:09:49 +0200

    First Commit

---
+++ Listas/src/main/java/com/aem/listas/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.aem.listas;
+
+/**
+ *
+ * @author AEM by Alejandro Esteban Martinez de la Casa
+ * @version 1.0
+ * Created on 16 sept 2024
+ *
+ */
+public class Main {
+    public static void main (String[] args){
+        
+    }
+}
