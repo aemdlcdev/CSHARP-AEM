854eebb609f2abda818b6c8f9633c4f05cafaab2
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-09-23 12:29:22 +0200

    .

--- UD1/Ejercicios1/Ejercicios1/Program.cs
+++ UD1/Ejercicios1/Ejercicios1/Program.cs
@@ -10,6 +10,30 @@ namespace Ejercicios1
     {
         static void Main(string[] args)
         {
+            /*
+             * Usuario introduce numeros y el programa cuenta los positivos
+             * 
+             */
+            int positivos = 0;
+            
+            int numTecleado = 0;
+            Boolean interruptor = true;
+            while (interruptor) {
+                Console.WriteLine("Introduzca un numero" + ", introduce 0 para mostrar los positivos tecleados");
+                numTecleado= int.Parse(Console.ReadLine());
+                if (numTecleado > 0)
+                {
+                    positivos += 1;
+                }
+                else if (numTecleado == 0)
+                {
+                    interruptor = false;
+                }
+            
+        }
+            Console.WriteLine("Total de numeros positivos tecleados: " + positivos);
+            Console.ReadKey(); //Esto lo uso para que no se cierre la consola cuando termine la ejecucion
+
         }
     }
 }
