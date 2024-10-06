5f429d8e3e660a1a09c04f4b15f16a1e1707b4bd
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-06 20:43:14 +0200

    tablero added

---
+++ UD1/Tablero/Tablero/Operaciones.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablero
+{
+    internal class Operaciones
+    {
+
+        public static void MuestraMenu()
+        {
+            Console.WriteLine("Seleccione una operacion");
+            Console.WriteLine("1: Derecha");
+            Console.WriteLine("2: Izquierda");
+            Console.WriteLine("3: Arriba");
+            Console.WriteLine("4: Abajo");
+            Console.WriteLine("5: Salir");
+        }
+
+        public static int LeeOpcion()
+        {
+            try
+            {
+                int opcion = 0;
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    return opcion;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Opcion incorrecta" + ex.Message);
+            }
+            return 0;
+        }
+
+        public static void MuestraArray(string[,] array)
+        {
+            // GetLength(0) obtiene el número de filas, GetLength(1) obtiene el número de columnas
+            for (int i = 0; i < array.GetLength(0); i++) // Recorre las filas
+            {
+                for (int j = 0; j < array.GetLength(1); j++) // Recorre las columnas
+                {
+                    Console.Write(array[i, j] + " "); // Imprime cada elemento de la matriz
+                }
+                Console.WriteLine(); // Salto de línea al final de cada fila
+            }
+        }
+
+
+    }
+}
---
+++ UD1/Tablero/Tablero/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            #region TABLERO
+            string[,] tablero = new string[4, 4];
+
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    tablero[i, j] = "X";
+                }
+            }
+            tablero[0, 0] = "0";
+            Operaciones.MuestraArray(tablero);
+            #endregion
+
+            Console.WriteLine("");
+
+            #region MENU
+            bool esValido = false; 
+
+            do
+            {
+                Operaciones.MuestraMenu(); 
+
+                int operacion = Operaciones.LeeOpcion(); 
+
+                switch (operacion)
+                {
+                    case 1: //Derecha
+                        tablero[0, 0] = "X";
+                        tablero[0, 0+1] = "0";
+                        Operaciones.MuestraArray(tablero);
+                        Console.WriteLine("Opción 1 seleccionada.");
+                      
+                        break;
+                    case 2: //Izquierda
+                        Console.WriteLine("Opción 2 seleccionada.");
+                        
+                        break;
+                    case 3: //Arriba
+                        Console.WriteLine("Opción 3 seleccionada.");
+                        
+                        break;
+                    case 4: //Abajo
+                        Console.WriteLine("Opción 4 seleccionada.");
+                        
+                        break;
+                    case 5:
+                        Console.WriteLine("Adios"); 
+                        esValido = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no reconocida. Intenta de nuevo."); 
+                        break;
+                }
+
+            } while (!esValido);
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
