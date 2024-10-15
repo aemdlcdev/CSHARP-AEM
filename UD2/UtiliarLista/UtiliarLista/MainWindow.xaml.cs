65df79a373482370dc6e1b4f2c32c096dde3ab68
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-15 13:04:18 +0200

    listasuse added

---
+++ UD2/UtiliarLista/UtiliarLista/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UtiliarLista
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAniadir_Click(object sender, RoutedEventArgs e)
+
+        {
+            if(string.IsNullOrEmpty(txtTituloPelicula.Text))
+            {
+                MessageBox.Show("Debe ingresar un titulo de pelicula");
+                return;
+            }
+            //Añadir elementos a la lista
+            cmbPeliculas.Items.Add(txtTituloPelicula.Text);
+
+            txtTituloPelicula.Text = "";
+        }
+
+        private void cmbPeliculas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lstPeliculas.Items.Add(cmbPeliculas.SelectedItem);
+        }
+    }
+}
