09650e9efd4e9059ed146c18690640799f51e899
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-15 12:47:06 +0200

    proyectos added

---
+++ UD2/CambiarFondoColor/CambiarFondoColor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CambiarFondoColor
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAzul_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void btnRojo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void btnVerde_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void btnAmarillo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = new SolidColorBrush(Colors.Yellow);
+        }
+    }
+}
---
+++ UD2/CopiarValor/CopiarValor/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CopiarValor
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopiar_Click(object sender, RoutedEventArgs e)
+        {
+            string valor = tbxValor.Text;
+            tbxResultado.Text = valor;
+        }
+    }
+}
