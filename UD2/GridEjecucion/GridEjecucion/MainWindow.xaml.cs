fb9c0fc218708d42db772a609238420c11897e4b
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-15 13:43:13 +0200

    ejemplos added

---
+++ UD2/GridEjecucion/GridEjecucion/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridEjecucion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            contenedor.RowDefinitions.Add(new RowDefinition());
+            contenedor.RowDefinitions.Add(new RowDefinition());
+            contenedor.RowDefinitions.Add(new RowDefinition());
+
+            contenedor.ColumnDefinitions.Add(new ColumnDefinition());
+            contenedor.ColumnDefinitions.Add(new ColumnDefinition());
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Button boton = new Button();
+                    boton.Width = 100;
+                    boton.Height = 50;
+                    boton.Content = "Botón " + i + ", " + j;
+                    boton.HorizontalAlignment = HorizontalAlignment.Left;
+                    boton.VerticalAlignment = VerticalAlignment.Top;
+
+                    //La colocación de los controles depende
+                    //de su alineación y del margen de cada control
+
+                    boton.Margin = new Thickness(20 + i * 110, 10 + j * 55, 0, 0);
+                    contenedor.Children.Add(boton);
+                }
+            }
+
+        }
+    }
+}
---
+++ UD2/GridEjecucion2/GridEjecucion2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridEjecucion2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button boton = new Button();
+                    boton.Content = "Botón " + i + ", " + j;
+                    boton.HorizontalAlignment =
+                    HorizontalAlignment.Left;
+                    boton.VerticalAlignment = VerticalAlignment.Top;
+                    //La colocación de los controles depende de los métodos
+                    //estáticos SetRow y SetColumn de la clase Grid
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+                    contenedor.Children.Add(boton);
+                }
+            }
+        }
+    }
+}
