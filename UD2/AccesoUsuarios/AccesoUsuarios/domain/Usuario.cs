08380a9a77a0cf4de6829de7542dde6c460bf61c
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-24 10:07:20 +0200

    accesoausuarios added

---
+++ UD2/AccesoUsuarios/AccesoUsuarios/domain/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoUsuarios
+{
+    internal class Usuario
+    {
+        public String Nombre { get; set; }
+        public String Contrasena { get; set; }
+
+        public Usuario(String nombre, String contrasena)
+        {
+            Nombre = nombre;
+            Contrasena = contrasena;
+        }
+
+        public override string ToString()
+        {
+            return Nombre + " " +Contrasena;
+        }
+
+    }
+}
