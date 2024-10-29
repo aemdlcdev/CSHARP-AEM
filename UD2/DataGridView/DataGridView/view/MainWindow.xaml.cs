d73f2b2a9fc400964da1fd1efa62c24c33024193
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-10-29 12:40:45 +0100

    datagridview terminado

--- UD2/DataGridView/DataGridView/view/MainWindow.xaml.cs
+++ UD2/DataGridView/DataGridView/view/MainWindow.xaml.cs
@@ -24,26 +24,50 @@ namespace DataGridView
 
         private void btnAgregarPersona_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txtNombre.Text) || string.IsNullOrWhiteSpace(txtApellidos.Text) || string.IsNullOrWhiteSpace(txtEdad.Text))
+            if(btnAgregarPersona.Content.Equals("Añadir persona"))
             {
-                MessageBox.Show("Por favor, complete todos los campos.", "Campos vacíos", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
+                if (string.IsNullOrWhiteSpace(txtNombre.Text) || string.IsNullOrWhiteSpace(txtApellidos.Text) || string.IsNullOrWhiteSpace(txtEdad.Text))
+                {
+                    MessageBox.Show("Por favor, complete todos los campos.", "Campos vacíos", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                if (!int.TryParse(txtEdad.Text, out int edad))
+                {
+                    MessageBox.Show("Por favor, ingrese una edad válida.", "Edad inválida", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                string nombre = txtNombre.Text;
+                string apellidos = txtApellidos.Text;
+
+                Persona nuevaPersona = new Persona(nombre, apellidos, edad);
+                if (VerificarExistencia(nuevaPersona))
+                {
+                    MessageBox.Show("La persona ya existe", "Persona existente", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                else
+                {
+                    listaPersonas.Add(nuevaPersona);
+                    dataGridPersonas.Items.Refresh();
+                    start();
+                }
+            }
+            else if (btnAgregarPersona.Content.Equals("Guardar cambios"))
+            {
+                if (dataGridPersonas.SelectedItem is Persona personaSeleccionada)
+                {
+                    
+                    personaSeleccionada.Nombre = txtNombre.Text;
+                    personaSeleccionada.Apellidos = txtApellidos.Text;
+                    personaSeleccionada.Edad = int.Parse(txtEdad.Text);
+
+                    dataGridPersonas.Items.Refresh();
+                    start();
+                }
             }
 
-            if (!int.TryParse(txtEdad.Text, out int edad))
-            {
-                MessageBox.Show("Por favor, ingrese una edad válida.", "Edad inválida", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-
-            string nombre = txtNombre.Text;
-            string apellidos = txtApellidos.Text;
-
-            Persona nuevaPersona = new Persona(nombre, apellidos, edad);
-            listaPersonas.Add(nuevaPersona);
-
-            dataGridPersonas.Items.Refresh();
-            start();
         }
 
         private void start()
@@ -52,6 +76,9 @@ namespace DataGridView
             txtApellidos.Text = "";
             txtEdad.Text = "";
             btnAgregarPersona.IsEnabled = true;
+            btnAgregarPersona.Content = "Añadir persona";
+            btnEliminar.IsEnabled = true;
+            btnModificar.IsEnabled = true;
         }
 
         private void dataGridPersonas_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -72,6 +99,8 @@ namespace DataGridView
             }
         }
 
+
+
         private void btnEliminar_Click(object sender, RoutedEventArgs e)
         {
             if (MessageBox.Show("¿Está seguro que quiere eliminar esta persona?", "Eliminar", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes && dataGridPersonas.SelectedItem is Persona personaSeleccionada)
@@ -84,7 +113,28 @@ namespace DataGridView
 
         private void btnModificar_Click(object sender, RoutedEventArgs e)
         {
-            
+            if(dataGridPersonas.SelectedItem is Persona personaSeleccionada)
+            {
+                btnAgregarPersona.Content = "Guardar cambios";
+                btnEliminar.IsEnabled = false;
+                btnModificar.IsEnabled = false;
+                btnAgregarPersona.IsEnabled = true;
+            }
+
         }
+
+        private bool VerificarExistencia(Persona persona)
+        { 
+            bool existe = false;
+            string name = persona.Nombre;
+            string lastName = persona.Apellidos;
+            int age = persona.Edad;
+            if(listaPersonas.Exists(p => p.Nombre == name && p.Apellidos == lastName && p.Edad == age))
+            {
+                existe = true;
+            }
+            return existe;
+        }
+
     }
 }
