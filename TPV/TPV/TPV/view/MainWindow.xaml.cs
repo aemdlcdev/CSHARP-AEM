48c84a9a060d59cd03398513bc7ae23702bcf055
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-12-08 20:17:29 +0100

    terminado

--- TPV/TPV/TPV/view/MainWindow.xaml.cs
+++ TPV/TPV/TPV/view/MainWindow.xaml.cs
@@ -353,7 +353,7 @@ namespace TPV
 
             Usuario usuario = new Usuario(id, nombre, contraseñaEncriptada, idRol);
             usuario.ModificarUsuario(usuario);
-
+            MessageBox.Show("Usuario modificado correctamente!", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
             RefrescarUsuarios();
         }
 
@@ -436,15 +436,14 @@ namespace TPV
 
             if (clienteSeleccionado != null)
             {
-                // Recupero la cuenta del cliente actual
-                if (cuentasClientes.TryGetValue(clienteSeleccionado.codCliente, out CuentaCliente cuenta))
+                // Recupero la cuenta del cliente actual o creo una nueva
+                if (!cuentasClientes.TryGetValue(clienteSeleccionado.codCliente, out CuentaCliente cuenta))
                 {
-                    cuentaCliente = cuenta;
+                    cuenta = new CuentaCliente(clienteSeleccionado);
+                    cuentasClientes[clienteSeleccionado.codCliente] = cuenta; // Agregar al diccionario
                 }
-                else
-                {
-                    cuentaCliente = new CuentaCliente(clienteSeleccionado);
-                }
+
+                cuentaCliente = cuenta;
 
                 // Establecer el saldo inicial del cliente (incluyendo operaciones previas)
                 currentValue = cuentaCliente.Total;
