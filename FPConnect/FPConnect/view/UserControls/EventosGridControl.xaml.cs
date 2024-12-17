2a02bb3f270693b56ef80ed6f5c54c13b2dd9c1f
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2024-12-17 19:41:54 +0100

    .

--- FPConnect/FPConnect/view/UserControls/EventosGridControl.xaml.cs
+++ FPConnect/FPConnect/view/UserControls/EventosGridControl.xaml.cs
@@ -24,5 +24,10 @@ namespace FPConnect.view.UserControls
         {
             InitializeComponent();
         }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
     }
 }
