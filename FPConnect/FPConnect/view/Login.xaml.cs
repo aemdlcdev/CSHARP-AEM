83454512cce1c240f27ae9bd7d97625bf82b4237
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2025-01-16 11:12:47 +0100

    .

--- FPConnect/FPConnect/view/Login.xaml.cs
+++ FPConnect/FPConnect/view/Login.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FPConnect.view
 {
@@ -26,7 +16,10 @@ namespace FPConnect.view
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            DragMove();
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
