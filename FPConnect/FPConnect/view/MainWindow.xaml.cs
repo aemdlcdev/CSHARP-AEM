d5117323cf7663e97b46ac615dbc2577454eac0d
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2025-01-20 12:58:26 +0100

    .

--- FPConnect/FPConnect/view/MainWindow.xaml.cs
+++ FPConnect/FPConnect/view/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace FPConnect
                 else
                 {
                     this.WindowState = WindowState.Maximized;
-
+                    
                     IsMaximize = true;
                 }
             }
