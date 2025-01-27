5e22f8046270aae6add8927c92bcaf83c36b3e83
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2025-01-27 19:31:07 +0100

    .

---
+++ CONSULTAS/Consultas copoa minihito/MiniHito1/domain/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHito1.domain
+{
+    internal class Empleado : Usuario
+    {
+
+        int idEmpleado { get; set; }
+        string nombre { get; set; }
+        string apellidos { get; set; }
+        double crs { get; set; }
+        int idUsuario { get; set; }
+        int idRol { get; set; }
+    }
+}
---
+++ CONSULTAS/Consultas copoa minihito/MiniHito1/domain/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHito1.domain
+{
+    internal class Usuario
+    {
+        int idUsuario { get; set; }
+        string nombre { get; set; }
+        string password { get; set; }
+    }
+}
