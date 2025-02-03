5f4aba93d65b78deccf801092faef25b8ccd8701
Aestebanmdlc20 <165804900+Aestebanmdlc20@users.noreply.github.com>
2025-02-03 11:03:54 +0100

    .

---
+++ REPORT/REPORT/view/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace REPORT
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataTable tabla1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Crear una tabla a partir de un DataTable que ya tenemos
+            tabla1 = new DataTable("DataTable1");
+            // Crear las columnas con los mismo nombres de campos del detatable creado en el crystal report
+            tabla1.Columns.Add("Name");
+            tabla1.Columns.Add("Age");
+            tabla1.Columns.Add("Adress");
+            tabla1.Columns.Add("Phone");
+
+            // Añadimos 100 registros para probar
+            
+
+
+
+        }
+    }
+}
